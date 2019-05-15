8a535393846b11963188a6b1ae9ae0723fde247b
Cameron Reikes <cameronreikes@gmail.com>
2019-05-15 16:23:57 -0700

    Implement smooth camera movement mimicking editor

--- src/nodes/viewer/Viewer.gd
+++ src/nodes/viewer/Viewer.gd
@@ -1,29 +1,43 @@
 extends Spatial
 
-export var lower_bound = 20.0
-export var upper_bound = 400.0
-const pan_speed = 0.4
-const zoom_speed = 20.0
+export var smoothing = 8.0
+export var pan_speed = 1.0
+export var zoom_speed = 35.0
+
+#var local_translate_accel: Vector3 = Vector3()
+#var local_translate_vel: Vector3 = Vector3()
+
+onready var target_translation: Vector3 = translation
+
+var num = 0
 
 func _ready():
+	set_physics_process(true)
 	set_process_input(true)
 
-func translate_object_local(to_translate: Vector3):
-	.translate_object_local(to_translate)
-	print($Camera.global_transform.origin.y)
-	if $Camera.global_transform.origin.y < lower_bound || $Camera.global_transform.origin.y > upper_bound:
-		.translate_object_local(-Vector3(0.0, to_translate.y, 0.0))
+func _physics_process(delta):
+#	var move_vector = (target_translation - translation)
+#	var error = move_vector.length()
+#	local_translate_vel += local_translate_accel
+#	print((target_translation - translation).length())
+	translation = (target_translation - translation) * smoothing * delta + translation
 
 func _input(event):
 	# pan movement with motion
 	if event is InputEventMouseMotion and Input.is_action_pressed("g_pan"):
-		translate_object_local(Vector3(event.relative.x, -event.relative.y, 0.0)*pan_speed)
+		target_translation += Vector3(event.relative.x, -event.relative.y, 0.0)*pan_speed
 	# show and hide mouse when panning
 	elif event.is_action_pressed("g_pan"):
 		Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
 	elif event.is_action_released("g_pan"):
 		Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
 	elif event.is_action_pressed("g_zoom_in"):
-		translate_object_local(($Camera.transform.origin).normalized() * -zoom_speed)
+#		print($Camera.global_transform.origin)
+		target_translation += get_zoom_direction() * -zoom_speed
+#		print(target_translation)
 	elif event.is_action_pressed("g_zoom_out"):
-		translate_object_local(($Camera.transform.origin).normalized() * zoom_speed)
+		target_translation += get_zoom_direction() * zoom_speed
+
+func get_zoom_direction():
+	var real_camera_pos = $Camera.global_transform.origin + (target_translation - translation)
+	return (real_camera_pos - target_translation).normalized()

c831c0cf2203a99d74b8fb9009dd8b325873ce48
Cameron Reikes <cameronreikes@gmail.com>
2019-05-16 11:22:55 -0700

    Add lower limit to camera height

--- src/nodes/viewer/Viewer.gd
+++ src/nodes/viewer/Viewer.gd
@@ -3,6 +3,7 @@ extends Spatial
 export var smoothing = 8.0
 export var pan_speed = 1.0
 export var zoom_speed = 35.0
+export var min_camera_height = 35.0
 
 onready var target_translation: Vector3 = translation
 
@@ -13,6 +14,10 @@ func _ready():
 func _physics_process(delta):
 	# smoothly interpolate translation
 	translation = (target_translation - translation) * smoothing * delta + translation
+	var new_camera_pos = get_camera_pos()
+	print(new_camera_pos)
+	if new_camera_pos.y <= min_camera_height:
+		target_translation.y += min_camera_height - new_camera_pos.y
 
 func _input(event):
 	# pan movement with motion
@@ -28,6 +33,9 @@ func _input(event):
 	elif event.is_action_pressed("g_zoom_out"):
 		target_translation += get_zoom_direction() * zoom_speed
 
-func get_zoom_direction():
-	var real_camera_pos = $Camera.global_transform.origin + (target_translation - translation)
+func get_camera_pos() -> Vector3:
+	return $Camera.global_transform.origin + (target_translation - translation)
+
+func get_zoom_direction() -> Vector3:
+	var real_camera_pos = get_camera_pos()
 	return (real_camera_pos - target_translation).normalized()

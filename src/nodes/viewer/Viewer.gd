04cb97da9c1ff15aca5474503906fb8531a014fa
Cameron Reikes <cameronreikes@gmail.com>
2019-05-24 09:29:28 -0700

    Convert to using transforms instead of position

--- src/nodes/viewer/Viewer.gd
+++ src/nodes/viewer/Viewer.gd
@@ -5,8 +5,8 @@ export var pan_speed = 1.0
 export var zoom_speed = 35.0
 export var min_camera_height = 35.0
 
-onready var target_translation: Vector3 = translation
-onready var base_translation: Vector3 = translation
+onready var target_transform: Transform = transform
+onready var base_transform: Transform = transform
 
 var g_pan_input: String = "g_pan"
 
@@ -18,31 +18,35 @@ func _ready():
 
 func _physics_process(delta):
 	# smoothly interpolate translation
-	translation = (target_translation - translation) * smoothing * delta + translation
+	# translation = (target_translation - translation) * smoothing * delta + translation
+	transform.origin = (target_transform.origin - transform.origin) * smoothing * delta + translation
 	var new_camera_pos = get_camera_pos()
 #	print(new_camera_pos)
 	if new_camera_pos.y <= min_camera_height:
-		target_translation.y += min_camera_height - new_camera_pos.y
+		target_transform.origin.y += min_camera_height - new_camera_pos.y
 
 func _input(event):
 	# pan movement with motion
-	if event is InputEventMouseMotion and Input.is_action_pressed(g_pan_input):
-		target_translation += Vector3(event.relative.x, -event.relative.y, 0.0)*pan_speed
+	if event is InputEventMouseMotion:
+		if Input.is_action_pressed(g_pan_input):
+			target_transform.origin += Vector3(event.relative.x, -event.relative.y, 0.0)*pan_speed
+
+	# rotate about point with motion
 	# show and hide mouse when panning
 	elif event.is_action_pressed(g_pan_input):
 		Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
 	elif event.is_action_released(g_pan_input):
 		Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
 	elif event.is_action_pressed("g_zoom_in"):
-		target_translation += get_zoom_direction() * -zoom_speed
+		target_transform.origin += get_zoom_direction() * -zoom_speed
 	elif event.is_action_pressed("g_zoom_out"):
-		target_translation += get_zoom_direction() * zoom_speed
+		target_transform.origin += get_zoom_direction() * zoom_speed
 	elif event.is_action_pressed("g_cam_reset"):
-		target_translation = base_translation
+		target_transform = base_transform
 
 func get_camera_pos() -> Vector3:
-	return $Camera.global_transform.origin + (target_translation - translation)
+	return $Camera.global_transform.origin + (target_transform.origin - translation)
 
 func get_zoom_direction() -> Vector3:
 	var real_camera_pos = get_camera_pos()
-	return (real_camera_pos - target_translation).normalized()
+	return (real_camera_pos - target_transform.origin).normalized()

f74bb3e48ddbfd4c4f0bd4590a1c4903899af495
Cameron Reikes <cameronreikes@gmail.com>
2019-05-26 16:25:22 -0700

    Get rid of useless ocean information in boat

--- src/assets/ships/Ship.gd
+++ src/assets/ships/Ship.gd
@@ -1,28 +1,15 @@
 extends RigidBody
 
-export (NodePath) var ocean_path setget set_ocean_path
 export var buoyancy_constant = 1.0
 
-onready var ocean_inst = get_node(ocean_path) as ocean
 onready var gravity = ProjectSettings.get("physics/3d/default_gravity")
 var base_height: float
-
-func set_ocean_path(new_ocean_path):
-	ocean_path = new_ocean_path
-	if has_node(new_ocean_path):
-		ocean_inst = get_node(new_ocean_path)
 
 func _ready():
 	base_height = translation.y
 
 func _integrate_forces(state: PhysicsDirectBodyState):
-	var ocean_position
-	var archimedes_force = 0.0
-	if ocean_inst != null:
-		ocean_position = ocean_inst.get_displace(translation)
-	else:
-		ocean_position = Vector3(translation.x, base_height, translation.z)
-	archimedes_force = base_height - translation.y
+	var archimedes_force = base_height - translation.y
 	# archimedes_force = base_height - ocean_position.y
 	if archimedes_force < 0.0:
 		archimedes_force = 0.0

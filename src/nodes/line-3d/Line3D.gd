e353b335b0da36ac4a815bd7450468d4578a06ec
Cameron Reikes <cameronreikes@gmail.com>
2019-06-08 10:58:32 -0700

    Add visual line to see ship progress

---
+++ src/nodes/line-3d/Line3D.gd
@@ -0,0 +1,26 @@
+extends ImmediateGeometry
+
+export var line_color = Color() setget set_line_color
+var target_position = Vector3() setget set_target_position
+
+func _ready():
+	set_as_toplevel(true)
+
+func set_line_color(new_line_color):
+	line_color = new_line_color
+#	update_line()
+
+func set_target_position(new_target_position):
+	target_position = new_target_position
+#	update_line()
+
+func _process(delta):
+	update_line()
+
+func update_line():
+	clear()
+	begin(Mesh.PRIMITIVE_LINES)
+	set_color(line_color)
+	add_vertex(get_parent().global_transform.origin)
+	add_vertex(target_position)
+	end()
